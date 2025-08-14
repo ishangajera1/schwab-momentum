d4f60eff9e96b933acb8167f4870e9591570e7a6
ishangajera1 <41835444+ishangajera1@users.noreply.github.com>
2025-08-13 22:21:23 -0400

    Update vite.config.js

--- web/vite.config.js
+++ web/vite.config.js
@@ -1,8 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
   plugins: [react()],
-  build: { outDir: "dist" },
+  root: path.resolve(__dirname),
+  build: {
+    outDir: path.resolve(__dirname, "dist")
+  }
 });
-
